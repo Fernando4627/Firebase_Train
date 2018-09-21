b9831cd3b7e9d7d3b8e4a9aa5ed7fc0686dc89a6
Fernando Aguilar <fernandoaguilar17@gmail.com>
2018-09-21 01:11:06 -0700

    Look Nice Now

--- assets/Javascript/app.js
+++ assets/Javascript/app.js
@@ -35,26 +35,26 @@ $(document).ready(function () {
         });
     });
     database.ref().on("child_added", function (childSnapschot) {
-        console.log(childSnapschot.val().train);
-        console.log(childSnapschot.val().destination);
-        console.log(childSnapschot.val().frequency);
-        console.log(childSnapschot.val().firstTime);
+        //console.log(childSnapschot.val().train);
+        //console.log(childSnapschot.val().destination);
+        //console.log(childSnapschot.val().frequency);
+        //console.log(childSnapschot.val().firstTime);
         tfrequency = childSnapschot.val().frequency;
         tfirstTime = childSnapschot.val().firstTime;
 
 
         var firstTimeConverted = moment(tfirstTime, "HH:mm").subtract(1, "years");
-        console.log(firstTimeConverted);
+        //console.log(firstTimeConverted);
         var currentTime = moment();
-        console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+        //console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
         var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-        console.log("DIFFERENCE IN TIME: " + diffTime);
+        //console.log("DIFFERENCE IN TIME: " + diffTime);
         var tRemainder = diffTime % tfrequency;
-        console.log(tRemainder);
+        //console.log(tRemainder);
         var minutesAway = tfrequency - tRemainder;
-        console.log("MINUTES TILL TRAIN: " + minutesAway);
+        //console.log("MINUTES TILL TRAIN: " + minutesAway);
         var nextTrain = moment().add(minutesAway, "minutes");
-        console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+        //console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
 
         $('.trains').append('<tr>'+'<td>' + childSnapschot.val().train + '</td>' +
             '<td>' + childSnapschot.val().destination + '</td>' +
